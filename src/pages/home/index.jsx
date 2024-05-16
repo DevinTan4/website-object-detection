b5703d24046852e4855cf6b041d60da0572651a9
DasonTiovino <codingdec14@gmail.com>
2024-05-16 18:43:05 +0700

    refactor: vertical align center cards

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -9,7 +9,9 @@ const HomePage = () => {
   useEffect(() => {
     const fetchMotorbikeCount = async () => {
       try {
-        const response = await fetch("http://localhost:8080/api/motorbike");
+        const response = await fetch(
+          "http://192.168.169.76:8000/api/motorbike"
+        );
         const data = await response.json();
         setMotorbikeCount(data.motorbike_count);
       } catch (error) {
@@ -19,7 +21,7 @@ const HomePage = () => {
 
     fetchMotorbikeCount();
 
-    const intervalId = setInterval(fetchMotorbikeCount, 5000);
+    const intervalId = setInterval(fetchMotorbikeCount, 3000);
 
     return () => clearInterval(intervalId);
   }, []);
@@ -27,7 +29,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchCarCount = async () => {
       try {
-        const response = await fetch("http://localhost:8080/api/car");
+        const response = await fetch("http://192.168.169.76:8000/api/car");
         const data = await response.json();
         setCarCount(data.car_count);
       } catch (error) {
@@ -37,17 +39,17 @@ const HomePage = () => {
 
     fetchCarCount();
 
-    const intervalId = setInterval(fetchCarCount, 5000);
+    const intervalId = setInterval(fetchCarCount, 3000);
 
     return () => clearInterval(intervalId);
   }, []);
 
   return (
-    <div className="lg:overflow-x-hidden">
+    <div className="lg:overflow-x-hidden h-full flex flex-col">
       {/* Header */}
       <header>
         {/* Navbar */}
-        <nav className="fixed top-0 left-0 right-0">
+        <nav className="">
           <div className="w-full h-[100px] flex justify-center items-center bg-[#f0f1f1] shadow-xl">
             <img src={praditaLogoImg} alt="PraditaLogo" className="w-[150px]" />
           </div>
@@ -55,7 +57,7 @@ const HomePage = () => {
       </header>
 
       {/* Main */}
-      <main className="mt-[100px] py-[35px] flex flex-col items-center gap-8 lg:flex-row lg:py-20 lg:justify-evenly">
+      <main className="flex flex-col items-center gap-8 lg:flex-row lg:justify-evenly h-full">
         {/* Gedung 1 */}
         <div className="w-[347px] h-[320px] bg-[#F04524] rounded-2xl shadow-2xl shadow-[#F8BA18] py-8 px-[63px] flex flex-col items-center gap-8 lg:w-[450px] lg:py-6">
           <h1 className="font-bold text-5xl lg:text-[54px]">Gedung 1</h1>
